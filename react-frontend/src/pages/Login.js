a55817c96055cb811c1d90ac56fa66863a58f31f
Andrew <andrew123709@gmail.com>
2023-09-19 22:05:26 -0400

    add forms frontend

--- react-frontend/src/pages/Login.js
+++ react-frontend/src/pages/Login.js
@@ -1,21 +1,38 @@
 import React from 'react';
+import NavBar from "../components/NavBar";
 
 function Login() {
     return (
         <div class="bg-slate-200 w-full h-full fixed">
-            <form method="post">
-                <div className="input-container">
-                    <label>Username </label>
-                    <input type="text" name="uname" required />
-                </div>
-                <div className="input-container">
-                    <label>Password </label>
-                    <input type="password" name="pass" required />
-                </div>
-                <div className="button-container">
-                    <input type="submit" />
-                </div>
-            </form>
+            <NavBar />
+            <div method="post" className="w-full max-w-xs absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                            Username
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="username" name="username" type="text" placeholder="Username" required/>
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <input
+                            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password" name="password" type="password" placeholder="******************" required/>
+                            <p className="text-red-500 text-xs italic">Please choose a password.</p>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="submit">
+                            Sign In
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
