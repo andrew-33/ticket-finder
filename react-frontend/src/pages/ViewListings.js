a677a8837ff0b2a4a69f8cad8f8cb49342ede331
Andrew <andrew123709@gmail.com>
2023-09-30 01:40:04 -0400

    add js to call post endpoint

--- react-frontend/src/pages/ViewListings.js
+++ react-frontend/src/pages/ViewListings.js
@@ -1,9 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import NavBar from "../components/NavBar";
 
+const saveEvent = async (event) => {
+    const url = process.env.REACT_APP_BACKEND_URL + "/api/add?name=" + event.name + "&url=" + event.url;
+    const response = await fetch(url, {
+        method: "POST",
+    });
+    console.log(response);
+}
+
 const parseEvents = async (response) => {
     let events = await response.json(); // await is important here
     events = events._embedded.events;
+    // parse the events array into simpler json without unused info
     const parsedEvents = events.map(event => (
         {
             name: event.name,
@@ -11,7 +20,7 @@ const parseEvents = async (response) => {
             url: event.url,
             venue: event._embedded.venues[0].name,
             min: event.priceRanges ? event.priceRanges[0].min : -1,
-            max: event.priceRanges ? event.priceRanges[0].max : -1,
+            max: event.priceRanges ? event.priceRanges[0].max : -1, // note: max=999 means 999+
             currency: event.priceRanges ? event.priceRanges[0].currency : ""
         }
     ));
@@ -61,6 +70,9 @@ export function ViewListings() {
                         <ul>
                             {events.map(event => (
                                 <li>
+                                    <button onClick={() => saveEvent(event)} class="bg-dark hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                                        Save
+                                    </button>
                                     <b>
                                         <a href={event.url}>
                                             {event.name}
