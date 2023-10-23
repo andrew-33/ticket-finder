8ad84e22e960e944ba59a47c6eec351e0f2aa93f
Andrew <andrew123709@gmail.com>
2023-10-22 21:46:58 -0400

    filtering search

--- react-frontend/src/pages/ViewListings.js
+++ react-frontend/src/pages/ViewListings.js
@@ -7,7 +7,7 @@ const saveEvent = async (event) => {
     currDate = currDate.toISOString().split('T')[0];
 
     const url = process.env.REACT_APP_BACKEND_URL + "/api/add?name=" + event.id + "&url=" + event.url + "&minPrice="
-                + event.min.toString() + "&maxPrice=" + event.max.toString() + "&date=" + currDate;
+        + event.min.toString() + "&maxPrice=" + event.max.toString() + "&date=" + currDate;
 
     const response = await fetch(url, {
         method: "POST",
@@ -39,15 +39,18 @@ const parseEvents = async (response) => {
 export function ViewListings() {
     const [loading, setLoading] = useState(false);
     const [events, setEvents] = useState([]);
+    const [url, setUrl] = useState("https://app.ticketmaster.com/discovery/v2/events?apikey=" + process.env.REACT_APP_API_KEY)
 
     useEffect(() => {
 
-        const params = ["marketId=102&", "segmentName=music&"]
-        let url = "https://app.ticketmaster.com/discovery/v2/events?";
+        let initialUrl = url;
+        const params = ["&marketId=102", "&segmentName=music"]
         for (const item of params) {
-            url += item;
+            if (!url.includes(item)) {
+                initialUrl += item;
+            }
         }
-        url += "apikey=" + process.env.REACT_APP_API_KEY;
+        setUrl(initialUrl);
 
         const load = async () => {
             setLoading(true);
@@ -59,34 +62,59 @@ export function ViewListings() {
             setLoading(false);
         }
 
-         load();
+        load();
 
     }, [])
+
+    async function applyFilters(e) {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+        console.log(formJson);
+
+        let filteredUrl = url;
+        filteredUrl += formJson.name ? "&keyword=" + formJson.name : "";
+        filteredUrl += formJson.city ? "&city=" + formJson.city : "";
+        // TODO: genre and venue ID
+
+        setLoading(true);
+        console.log(filteredUrl);
+
+        const response = await fetch(filteredUrl);
+        const result = await parseEvents(response);
+
+        setEvents(result);
+        setLoading(false);
+    }
 
     const buttonClass = "shadow appearance-none border rounded w-[10%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline";
 
     return (
         <div class="bg-slate-200 w-full h-full fixed overflow-auto">
-            <NavBar />
+            <NavBar/>
 
-                <br/><br/>
-                <h1 class="px-[5%]">Upcoming Events</h1>
+            <br/><br/>
+            <h1 class="px-[5%]">Upcoming Events</h1>
 
             <div className="py-8 px-[15%] ">
-                <div class="space-x-20">
-                    <h3>Filter</h3>
-                    <input className={buttonClass} id="name" name="name" type="text" placeholder="Name" />
-                    <input className={buttonClass} id="city" name="city" type="text" placeholder="City" />
-                    <input className={buttonClass} id="genre" name="genre" type="text" placeholder="Genre" />
-                    <input className={buttonClass} id="venue" name="venue" type="text" placeholder="Venue" />
-                </div>
-                <br/>
 
-                <button //onClick={} TODO
-                        className="bg-dark hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-                    Apply
-                </button>
-                <br/><br/>
+                <form onSubmit={applyFilters}>
+                    <div className="space-x-20">
+                        <h3>Filter</h3>
+                        <input className={buttonClass} id="name" name="name" type="text" placeholder="Name"/>
+                        <input className={buttonClass} id="city" name="city" type="text" placeholder="City"/>
+                        <input className={buttonClass} id="genre" name="genre" type="text" placeholder="Genre"/>
+                        <input className={buttonClass} id="venue" name="venue" type="text" placeholder="Venue"/>
+                    </div>
+                    <br/>
+
+                    <button type="submit" //onClick={applyFilters()}
+                            className="bg-dark hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                        Apply
+                    </button>
+                    <br/><br/>
+                </form>
 
                 {loading ? (<h1>loading...</h1>) : (
 
